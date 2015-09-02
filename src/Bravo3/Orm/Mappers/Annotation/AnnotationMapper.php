ab70f4546d8e0e6ee975962b0d4b6d2070ea7e17
Jordon <jordon@novatek.com.au>
2015-09-02 14:51:55 +1000

    Added Symfony namespace to Annotation mappers autoloader

--- src/Bravo3/Orm/Mappers/Annotation/AnnotationMapper.php
+++ src/Bravo3/Orm/Mappers/Annotation/AnnotationMapper.php
@@ -16,12 +16,28 @@ class AnnotationMapper implements MapperInterface
     public function __construct($paths = [])
     {
         if (!$paths) {
-            $paths[] = ['Bravo3\Orm\Annotations', __DIR__.'/../../../../'];
+            $paths = [
+                ['Bravo3\Orm\Annotations', __DIR__.'/../../../../'],
+                ['Symfony', __DIR__.'/../../../../../../../symfony/symfony/src/'],
+            ];
         }
 
         foreach ($paths as $path) {
             AnnotationRegistry::registerAutoloadNamespace($path[0], $path[1]);
         }
+    }
+
+    /**
+     * Adds a path to the annotation namespace autoloader
+     *
+     * @param string $namespace Base namespace (eg "Foo\Bar")
+     * @param string $path      Path to PSR-0 root folder (eg __DIR__."/../src/")
+     * @return $this
+     */
+    public function addAnnotationPath($namespace, $path)
+    {
+        AnnotationRegistry::registerAutoloadNamespace($namespace, $path);
+        return $this;
     }
 
     /**
